07b0ad245e46f0f45162d542c16df6bc100f92d3
deepred5 <948931744@qq.com>
2019-07-22 18:49:41 +0800

    update: 网络请求

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,22 @@
-#! /usr/bin/env node 
+#! /usr/bin/env node
 
 const program = require('commander');
+const axios = require('axios');
+
+// [type] 可选
+// <type> 必选
 
 program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+  .command('anime [day]')
+  .description('choose anime day')
+  .option('-l, --limit [amount]', 'limit amount', parseInt, 20)
+  .action(async (day, cmd) => {
+    try {
+      const { data } = await axios.get('https://bangumi.bilibili.com/web_api/timeline_global');
+    } catch (err) {
+    }
+  });
 
 program.parse(process.argv);
 
-if (program.debug) console.log(program.opts());
-console.log('pizza details:');
-if (program.small) console.log('- small pizza size');
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
 
